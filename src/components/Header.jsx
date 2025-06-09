39789db7724222ed3470e6b07585033a16548ea6
Abraham Efraim <75068289+reondaze-a@users.noreply.github.com>
2025-06-09 10:57:58 -0400

    add prelim css
    adding some css styles with tailwind

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import Navbar from './Navbar';
 
 export default function Header() {
   return (
-    <header className="bg-green-100 min-h-screen flex pt-50">
+    <header className="bg-green-100 flex w-full">
         <Navbar />
     </header>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 export default function Navbar() {
   return (
-    <nav className="bg-blue-500 max-h-10">
+    <nav className="bg-blue-500 max-h-10 w-full flex items-center justify-between px-4 py-2 rounded-b-md">
       <div className="flex justify-between gap-4">
         <a href="/" className="text-white text-lg font-bold">Projects</a>
         <a href="/about" className="text-white text-lg font-bold">About</a>
