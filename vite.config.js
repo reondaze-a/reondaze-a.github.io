bb21d6cec1f22c04e117eb77ded650ab5a2f2e38
Abraham Efraim <75068289+reondaze-a@users.noreply.github.com>
2025-08-19 11:12:28 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/",
   server: {
     port: 5000,
   }
