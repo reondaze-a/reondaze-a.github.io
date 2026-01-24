b54351f725d46cd196948b431d04e5d35d79e6f3
Abraham Efraim <75068289+reondaze-a@users.noreply.github.com>
2026-01-24 00:36:44 -0500

    Add Protein Per Dollar project to ProjectsGrid
    Added a new project, Protein Per Dollar, to the ProjectsGrid component. Included the project image asset and updated the import statements accordingly.

--- src/components/Projects/ProjectsGrid.jsx
+++ src/components/Projects/ProjectsGrid.jsx
@@ -2,6 +2,7 @@ import ProjectCard from "./ProjectCard";
 import spots_header from "../../assets/spots-project.png";
 import wtwr_header from "../../assets/what-to-wear.png";
 import church_header from "../../assets/church-demo.png";
+import protein_header from "../../assets/protein-per-dollar.png";
 
 export default function ProjectsGrid() {
     return (
@@ -29,6 +30,14 @@ export default function ProjectsGrid() {
                 image={church_header}
                 demoUrl="https://fresh-life-repo.vercel.app/"
                 githubUrl="https://github.com/reondaze-a/fresh-life-repo"
+            />
+            <ProjectCard 
+                title="Protein Per Dollar"
+                description="Protein Per Dollar is a frontend React application that helps users compare foods based on protein value relative to cost."
+                technologies={["React.js", "Tailwind", "FoodData Central API"]}
+                image={protein_header}
+                demoUrl="https://reondaze-a.github.io/final_project/#/"
+                githubUrl="https://github.com/reondaze-a/final_project"
             />
         </div>
     );
